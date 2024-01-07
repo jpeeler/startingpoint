c2fc2db97bf0e4ac5e982844b1771ebdcc0c842b
Jeff Peeler <jpeeler@gmail.com>
2024-01-07 18:41:02 -0500

    Revert "fix libdrm install issue"
    This reverts commit 1c3d03e819795edc423d782ac4885dc887f189bd.
    
    No longer required due to libdrm being bundled:
    https://github.com/solopasha/hyprlandRPM/issues/15

--- config/scripts/overrides.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "[start] Overriding RPMs..."
-wget https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-39/solopasha-hyprland-fedora-39.repo -P /etc/yum.repos.d/
-rpm-ostree override --experimental replace libdrm --from repo=copr:copr.fedorainfracloud.org:solopasha:hyprland
-echo "[done] Overriding RPMs..."
